f67d150a0cb5b7596be17a36f4d6ed966c0d0d38
Bryan0324 <34998621+Bryan0324@users.noreply.github.com>
2025-09-07 03:25:41 +0800

    測試長期token獲取

--- auto_push-to-threads.py
+++ auto_push-to-threads.py
@@ -1,9 +1,49 @@
 from threads.api import ThreadsAPI
 import sys
 import json
+
+import requests
+import json
+
+def refresh_token():
+    with open("secret.json", "r", encoding="utf-8") as file:
+        ori = file.read()
+        access_token = ori["access_token"]
+    if not access_token:
+        raise ValueError("❌ THREADS_ACCESS_TOKEN 環境變數未設定")
+
+    url = "https://graph.threads.net/refresh_access_token"
+    params = {
+        "grant_type": "th_refresh_token",
+        "access_token": access_token,
+    }
+
+    print("🔄 正在更新 Threads 長期存活 Token...")
+    resp = requests.get(url, params=params)
+    data = resp.json()
+
+    if "access_token" not in data:
+        raise RuntimeError(f"❌ 更新失敗: {data}")
+
+    new_token = data["access_token"]
+    expires_in = data.get("expires_in", 0)
+
+    print("✅ 更新成功")
+    print(f"新的 Access Token: {new_token}")
+    print(f"有效期: {expires_in // 86400} 天")
+
+    # 輸出到檔案（方便手動檢查/存放）
+    with open("secret.json", "w", encoding="utf-8") as f:
+        ori["access_token"] = new_token
+        json.dump(ori, f, indent=2, ensure_ascii=False)
+
+    return new_token
+
+
 # 從 git hook 傳進來的參數
 commit_message = sys.argv[1]
 changed_files = sys.argv[2:]
+refresh_token()
 with open("secret.json", "r", encoding="utf-8") as file:
     raw = json.loads(file.read())
     threads = ThreadsAPI(

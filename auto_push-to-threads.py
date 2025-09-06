53c60ac0ebffa10df3e9e21cbbf8311d0bc9e3ee
Bryan0324 <34998621+Bryan0324@users.noreply.github.com>
2025-09-06 21:45:32 +0800

    自動傳送測試

---
+++ auto_push-to-threads.py
@@ -0,0 +1,25 @@
+from threads.api import ThreadsAPI
+import sys
+import json
+# 從 git hook 傳進來的參數
+commit_message = sys.argv[1]
+changed_files = sys.argv[2:]
+with open("secret.json", "r", encoding="utf-8") as file:
+    raw = json.loads(file)
+    threads = ThreadsAPI(
+        user_id = raw["user_id"],
+        access_token=raw["access_token"],
+        app_secret=raw["app_secret"]
+    )
+
+# 組合發文內容
+text = "TOI初選錄取前每日一題競程"
+text += commit_message + "\n"
+text += "\n".join(changed_files)
+
+# 建立發文 Container
+media_json = threads.create_media_container(text=text)
+print("Container:", media_json)
+
+# 將 Container 發文
+threads.publish_container(media_json.get("id"))
